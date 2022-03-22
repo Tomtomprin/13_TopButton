f8a52b78ee11146fa793890f35dd67cd33913bca
Tomtomprin <97924842+Tomtomprin@users.noreply.github.com>
2022-03-22 11:10:54 +0900

    Fixed
    Safari対応記述追加

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const TopButton = document.querySelector('.page-top');
+const TopButton2 = document.querySelector('.page-top-2');
 
-function ScrollTop() {
+// jqueryなしver。Topボタン１Safari反応なし
+function ScrollTop1() {
     window.scrollTo({
         top: 0,
         // ↓補足："auto"だと、瞬時に移動する
@@ -9,4 +11,16 @@ function ScrollTop() {
     })
 };
 
-TopButton.onclick = ScrollTop;
+TopButton.onclick = ScrollTop1;
+
+
+//jquery使用。Topボタン２ Safari対応
+TopButton2.addEventListener('click', () => {
+    console.log('test');
+    $("body,html").animate({
+        scrollTop: 0
+
+        // 0.5秒かけてスクロール
+    }, 500);
+
+})
